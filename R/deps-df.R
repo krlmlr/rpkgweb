702c19083323ad3bc54b8c55afc39514c95e7b16
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-17 19:41:16 +0200

    deps_df

---
+++ R/deps-df.R
@@ -0,0 +1,30 @@
+#' Return dependencies in a package web as data frame
+#'
+#' This function computes all dependencies (internal and external) in a
+#' package web and returns the information as a data frame.
+#'
+#' @export
+deps_df <- function(web) UseMethod("deps_df")
+
+#' @export
+deps_df.default <- function(web) {
+  stop("Need object of class rpkgweb")
+}
+
+#' @export
+#' @importFrom dplyr bind_rows group_by do
+#' @importFrom devtools parse_deps
+deps_df.rpkgweb <- function(web) {
+  web %>%
+    lapply(names) %>%
+    lapply(intersect, c("depends", "imports", "suggests")) %>%
+    mapply(web, FUN = function(names, webitem) {
+      data.frame(package = webitem$package, dep_type = names,
+                 deps = unname(unlist(webitem[names])),
+                 stringsAsFactors = FALSE)
+    },
+    SIMPLIFY = FALSE) %>%
+    bind_rows %>%
+    group_by(package, dep_type) %>%
+    do(parse_deps(.$deps %>% debug_pipe)[, "name", drop = FALSE])
+}
