e62f1f640e6af716dc3668fa6dccfdef6d81ce5f
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-18 07:36:15 +0200

    get_web_root

---
+++ R/get-web-root.R
@@ -0,0 +1,9 @@
+#' Finds the root of a package web
+#'
+#' Currently returns the argument which defaults to the working directory.
+#'
+#' @param initial Starting directory of the search
+#' @export
+get_web_root <- function(initial = getwd()) {
+  initial
+}
