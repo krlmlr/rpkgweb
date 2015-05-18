9ca3744f4d9e6ae3515be3e1d0ee1400833a97f6
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-18 11:16:20 +0200

    test bumping

---
+++ tests/testthat/test-bump.R
@@ -0,0 +1,17 @@
+context("bump")
+
+library(magrittr)
+
+test_that("formatting", {
+  expect_equal(get_sprintf_format("0.0", 2), "%s.%s")
+  expect_equal(get_sprintf_format("0.0-0", 2), "%s.%s")
+  expect_equal(get_sprintf_format("0.0-0", 3), "%s.%s-%s")
+  expect_equal(get_sprintf_format("0.0-0", 5), "%s.%s-%s.%s.%s")
+})
+
+test_that("increasing", {
+  expect_equal(increase_version("0.0", 2, "0.0"), "0.1")
+  expect_equal(increase_version("0.0-0", 2, "0.0-0"), "0.1")
+  expect_equal(increase_version("0.0-10", 3, "0.0-0"), "0.0-11")
+  expect_equal(increase_version("0.0-10", 5, "0.0-0"), "0.0-10.0.1")
+})
