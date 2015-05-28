d6e80993a616a8bdc641c9208c1e7e2d48a84c1c
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-29 00:11:23 +0200

    read_dcf and write_dcf from devtools

--- R/bump.R
+++ R/bump.R
@@ -60,6 +60,22 @@ get_sprintf_format <- function(format, component) {
   paste(c("", format_split, ""), collapse = "%s")
 }
 
-"%:::%" <- function(p, f) {
-  get(f, envir = asNamespace(p))
+read_dcf <- function(path) {
+  fields <- colnames(read.dcf(path))
+  as.list(read.dcf(path, keep.white = fields)[1, ])
+}
+
+write_dcf <- function(path, desc) {
+  desc <- unlist(desc)
+  # Add back in continuation characters
+  desc <- gsub("\n[ \t]*\n", "\n .\n ", desc, perl = TRUE, useBytes = TRUE)
+  desc <- gsub("\n \\.([^\n])", "\n  .\\1", desc, perl = TRUE, useBytes = TRUE)
+
+  text <- paste0(names(desc), ": ", desc, collapse = "\n")
+
+  if (substr(text, nchar(text), 1) != "\n") {
+    text <- paste0(text, "\n")
+  }
+
+  cat(text, file = path)
 }
