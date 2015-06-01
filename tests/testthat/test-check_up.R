c86d350030b727fe2bc873a5ad57847b46d411cf
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-01 23:29:03 +0200

    much easier with mocking

--- tests/testthat/test-check_up.R
+++ tests/testthat/test-check_up.R
@@ -28,52 +28,31 @@ test_that("check_up acceptance test", {
           if (n %in% loadedNamespaces()) devtools::unload(n)
         }
 
-        local({
-          a_source_file <- file.path(web$packages[[1]]$path, "R", "a.R")
-          writeLines("thisWillTriggerAnError", a_source_file)
-          on.exit(unlink(a_source_file), add = TRUE)
-
-          expect_message(devtools::uninstall(web$packages[[1]], quiet = TRUE),
-                         lib_dir)
+        expect_message(devtools::uninstall(web$packages[[1]], quiet = TRUE),
+                       lib_dir)
+        with_mock(
+          `devtools::install` = function(x, ...) stop("devtools::install"),
           expect_error(check_up(web$packages[[1]]$package, web, quiet = TRUE),
-                         "thisWillTriggerAnError")
-          devtools::unload(web$packages[[1]]$package)
+                       "devtools::install")
+        )
 
-          for (n in names(web$packages)[-1:-2]) {
-            expect_error(check_up(n, web, quiet = TRUE), "Command failed", info = n)
-          }
-        })
+        devtools::unload(web$packages[[1]]$package)
 
-        for (n in names(web$packages)) {
-          expect_true(check_up(n, web, quiet = TRUE), info = n)
-          if (n %in% loadedNamespaces()) devtools::unload(n)
+        with_mock(
+          `devtools::test` = function(pkg, reporter, ...) {
+            expect_equal(reporter, "stop")
+            stop("devtools::test")
+          },
+          expect_error(check_up(web$packages[[1]]$package, web, quiet = TRUE),
+                       "devtools::test")
+        )
+
+        # No unloading here
+        #devtools::unload(web$packages[[1]]$package)
+
+        for (n in names(web$packages)[-1:-2]) {
+          expect_error(check_up(n, web, quiet = TRUE), "Command failed", info = n)
         }
-
-        local({
-          a_test_file <- file.path(web$packages[[1]]$path, "tests", "testthat", "test-fail.R")
-          writeLines("thisWillTriggerAnError", a_test_file)
-          on.exit(unlink(a_test_file), add = TRUE)
-
-          expect_message(devtools::uninstall(web$packages[[1]], quiet = TRUE),
-                         lib_dir)
-          expect_error(check_up(web$packages[[1]]$package, web, quiet = TRUE),
-                       "thisWillTriggerAnError")
-          devtools::unload(web$packages[[1]]$package)
-
-          for (n in names(web$packages)[-1:-2]) {
-            expect_error(check_up(n, web, quiet = TRUE), "Command failed", info = n)
-          }
-
-          writeLines(
-            c(
-              "context('fail')",
-              "test_that('fails', { expect_true(FALSE) })"
-            ),
-            a_test_file)
-          expect_error(check_up(web$packages[[1]]$package, web, quiet = TRUE),
-                       "Test failed")
-          devtools::unload(web$packages[[1]]$package)
-        })
 
         for (n in names(web$packages)) {
           expect_true(check_up(n, web, quiet = TRUE), info = n)
