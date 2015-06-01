427bae29fcb5802906d6d9ac7a0a9d394f914a97
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-01 22:31:31 +0200

    test with error

--- tests/testthat/test-check_up.R
+++ tests/testthat/test-check_up.R
@@ -13,8 +13,24 @@ local({
         web <- rpkgweb()
 
         for (n in names(web$packages)) {
-          expect_message(check_up(n, web, quiet = TRUE),
-                         "Package .* updated", label = n)
+          expect_true(check_up(n, web, quiet = TRUE), info = n)
+        }
+
+        for (n in names(web$packages)) {
+          expect_false(check_up(n, web, quiet = TRUE), info = n)
+        }
+
+        a_source_file <- file.path(web$packages[[1]]$path, "R", "a.R")
+        writeLines("thisWillTriggerAnError", a_source_file)
+        on.exit(unlink(a_source_file), add = TRUE)
+
+        expect_message(devtools::uninstall(web$packages[[1]], quiet = TRUE),
+                       lib_dir)
+        expect_error(check_up(web$packages[[1]]$package, web, quiet = TRUE),
+                       "Command failed")
+
+        for (n in names(web$packages)[-1:-2]) {
+          expect_error(check_up(n, web, quiet = TRUE), "Command failed", info = n)
         }
       })
     )
