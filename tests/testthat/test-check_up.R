b9aaa46fe51b9d679ea452addf678f2ff62de1a3
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-01 22:32:01 +0200

    local

--- tests/testthat/test-check_up.R
+++ tests/testthat/test-check_up.R
@@ -20,18 +20,20 @@ local({
           expect_false(check_up(n, web, quiet = TRUE), info = n)
         }
 
-        a_source_file <- file.path(web$packages[[1]]$path, "R", "a.R")
-        writeLines("thisWillTriggerAnError", a_source_file)
-        on.exit(unlink(a_source_file), add = TRUE)
+        local({
+          a_source_file <- file.path(web$packages[[1]]$path, "R", "a.R")
+          writeLines("thisWillTriggerAnError", a_source_file)
+          on.exit(unlink(a_source_file), add = TRUE)
 
-        expect_message(devtools::uninstall(web$packages[[1]], quiet = TRUE),
-                       lib_dir)
-        expect_error(check_up(web$packages[[1]]$package, web, quiet = TRUE),
-                       "Command failed")
+          expect_message(devtools::uninstall(web$packages[[1]], quiet = TRUE),
+                         lib_dir)
+          expect_error(check_up(web$packages[[1]]$package, web, quiet = TRUE),
+                         "Command failed")
 
-        for (n in names(web$packages)[-1:-2]) {
-          expect_error(check_up(n, web, quiet = TRUE), "Command failed", info = n)
-        }
+          for (n in names(web$packages)[-1:-2]) {
+            expect_error(check_up(n, web, quiet = TRUE), "Command failed", info = n)
+          }
+        })
       })
     )
   )
