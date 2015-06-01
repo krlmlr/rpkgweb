9b1ac34b28eab82d44cadc01975cad8353491cbe
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-01 21:30:18 +0200

    add test, currently failing

---
+++ tests/testthat/test-check_up.R
@@ -0,0 +1,18 @@
+context("check_up")
+
+local({
+  lib_dir <- normalizePath(".lib", mustWork = FALSE)
+  dir.create(lib_dir)
+  on.exit(unlink(lib_dir, recursive = TRUE, force = TRUE), add = TRUE)
+
+  devtools::in_dir(
+    "test_web",
+    local({
+      web <- rpkgweb()
+
+      for (n in names(web$packages)) {
+        expect_message(check_up(n, web), "Package .* updated", label = n)
+      }
+    })
+  )
+})
