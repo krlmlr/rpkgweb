fa5f348b08f92f35abc4db3ec774037d66c278b3
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-01 23:14:49 +0200

    test behavior with failing test

--- tests/testthat/test-check_up.R
+++ tests/testthat/test-check_up.R
@@ -43,6 +43,42 @@ test_that("check_up acceptance test", {
             expect_error(check_up(n, web, quiet = TRUE), "Command failed", info = n)
           }
         })
+
+        for (n in names(web$packages)) {
+          expect_true(check_up(n, web, quiet = TRUE), info = n)
+          if (n %in% loadedNamespaces()) devtools::unload(n)
+        }
+
+        local({
+          a_test_file <- file.path(web$packages[[1]]$path, "tests", "testthat", "test-fail.R")
+          writeLines("thisWillTriggerAnError", a_test_file)
+          on.exit(unlink(a_test_file), add = TRUE)
+
+          expect_message(devtools::uninstall(web$packages[[1]], quiet = TRUE),
+                         lib_dir)
+          expect_error(check_up(web$packages[[1]]$package, web, quiet = TRUE),
+                       "thisWillTriggerAnError")
+          devtools::unload(web$packages[[1]]$package)
+
+          for (n in names(web$packages)[-1:-2]) {
+            expect_error(check_up(n, web, quiet = TRUE), "Command failed", info = n)
+          }
+
+          writeLines(
+            c(
+              "context('fail')",
+              "test_that('fails', { expect_true(FALSE) })"
+            ),
+            a_test_file)
+          expect_error(check_up(web$packages[[1]]$package, web, quiet = TRUE),
+                       "Test failed")
+          devtools::unload(web$packages[[1]]$package)
+        })
+
+        for (n in names(web$packages)) {
+          expect_true(check_up(n, web, quiet = TRUE), info = n)
+          if (n %in% loadedNamespaces()) devtools::unload(n)
+        }
       })
     )
   )
