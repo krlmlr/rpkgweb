2f2aaf1a13c7d740b5ceb75ab91b0c47265f57a1
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-01 23:51:54 +0200

    test bumping package version

--- tests/testthat/test-bump.R
+++ tests/testthat/test-bump.R
@@ -14,4 +14,16 @@ test_that("increasing", {
   expect_equal(increase_version("0.0-10", 3, "0.0-0"), "0.0-11")
   expect_equal(increase_version("0.0-10", 5, "0.0-0"), "0.0-10.0.1")
   expect_equal(increase_version("0.0", 1, "0.0"), "1.0")
+  expect_equal(increase_version("0.2", 1, "0.0"), "1.0")
+})
+
+test_that("bumping", {
+  pkg_path <- "test_web/AA"
+  desc <- readLines(file.path(pkg_path, "DESCRIPTION"))
+  on.exit(writeLines(desc, file.path(pkg_path, "DESCRIPTION")), add = TRUE)
+  expect_message(bump(pkg_path), "bumped to version 0.1")
+  expect_message(bump(pkg_path, 3), "bumped to version 0.1-1")
+  expect_message(bump(pkg_path, 4), "bumped to version 0.1-1.1")
+  expect_message(bump(pkg_path), "bumped to version 0.2")
+  expect_message(bump(pkg_path, 1), "bumped to version 1.0")
 })
