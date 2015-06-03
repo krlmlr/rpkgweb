f5ccceb519d93838dd2ad9d487127cc3da97488c
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-03 23:31:32 +0200

    need different order

--- tests/testthat/test-make.R
+++ tests/testthat/test-make.R
@@ -3,20 +3,21 @@ context("make")
 envvar <- function() {
   # Instruct the Makefile where to load the package from
   pkg_path <- subset(devtools::loaded_packages(), package == "rpkgweb")$path
-  if (file.path(pkg_path, "tests", "testthat") == normalizePath(".", winslash = "/")) {
+  ret <- if (file.path(pkg_path, "tests", "testthat", "test_web") == normalizePath(".", winslash = "/")) {
     # Loaded by devtools, need patched version
-    list(RPKGWEB_QUALIFY = sprintf("devtools::load_all('%s'); ", pkg_path))
+    list(RPKGWEB_QUALIFY = sprintf("devtools::load_all('%s');", pkg_path))
   } else {
     # Installed package in R CMD check -- reset R_TESTS which points to an invalid path
     list(R_TESTS = "")
   }
+  ret
 }
 
 test_that("creation of Makefile", {
-  devtools::with_envvar(
-    envvar(),
-    devtools::in_dir(
-      "test_web",
+  devtools::in_dir(
+    "test_web",
+    devtools::with_envvar(
+      envvar(),
       local({
         web <- rpkgweb()
 
