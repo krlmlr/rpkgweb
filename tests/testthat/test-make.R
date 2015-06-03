cae14c88babc716d62554f1310a2b97d36d4611e
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-03 23:03:02 +0200

    extract function

--- tests/testthat/test-make.R
+++ tests/testthat/test-make.R
@@ -1,6 +1,6 @@
 context("make")
 
-test_that("creation of Makefile", {
+envvar <- function() {
   # Instruct the Makefile where to load the package from
   pkg_path <- subset(devtools::loaded_packages(), package == "rpkgweb")$path
   if (file.path(pkg_path, "tests", "testthat") == normalizePath(".", winslash = "/")) {
@@ -10,13 +10,13 @@ test_that("creation of Makefile", {
     # Installed package in R CMD check -- reset R_TESTS which points to an invalid path
     envvar <- list(R_TESTS = "")
   }
+}
 
+test_that("creation of Makefile", {
   devtools::in_dir(
     "test_web",
     devtools::with_envvar(
-      # Use load_all() instead of qualification :: to access test version
-      # of package instead of installed version
-      envvar,
+      envvar(),
       local({
         web <- rpkgweb()
 
