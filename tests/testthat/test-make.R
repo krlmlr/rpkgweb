56123ad3db046a0b1b86fe6f1499363db6a1e4de
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-03 20:11:24 +0200

    testing make uses local version (not the installed version)

--- tests/testthat/test-make.R
+++ tests/testthat/test-make.R
@@ -3,21 +3,26 @@ context("make")
 test_that("creation of Makefile", {
   devtools::in_dir(
     "test_web",
-    local({
-      web <- rpkgweb()
+    devtools::with_envvar(
+      # Use load_all() instead of qualification :: to access test version
+      # of package instead of installed version
+      list(RPKGWEB_QUALIFY = sprintf("devtools::load_all('%s'); ", devtools::as.package(".")$path)),
+      local({
+        web <- rpkgweb()
 
-      write_makefile(web)
-      on.exit(file.remove("Makefile"), add = TRUE)
+        write_makefile(web)
+        on.exit(file.remove("Makefile"), add = TRUE)
 
-      expect_message(write_makefile(), "unchanged")
+        expect_message(write_makefile(), "unchanged")
 
-      res <- system2("make", "-n", stdout = TRUE, stderr = TRUE)
-      expect_null(attr(res, "status"))
+        res <- system2("make", "-n", stdout = TRUE, stderr = TRUE)
+        expect_null(attr(res, "status"))
 
-      expect_true(any(grepl("unchanged", res)))
-      for (n in names(web$packages)) {
-        expect_true(any(grepl(sprintf("check_up.*%s", n), res)), info = n)
-      }
-    })
+        expect_true(any(grepl("unchanged", res)))
+        for (n in names(web$packages)) {
+          expect_true(any(grepl(sprintf("check_up.*%s", n), res)), info = n)
+        }
+      })
+    )
   )
 })
