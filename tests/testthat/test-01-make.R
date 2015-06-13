ac69f548854d6778ba26bd4a7c69323e31ec53f4
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-14 00:29:39 +0200

    add test with target_dir and lib_dir

--- tests/testthat/test-01-make.R
+++ tests/testthat/test-01-make.R
@@ -62,3 +62,14 @@ test_that("execution of Makefile for other lib", {
   # Packages are not installed after running
   expect_false(any((web %>% names) %in% rownames(installed.packages())))
 })
+
+test_that("execution of Makefile for different target_dir and other lib", {
+  web <- rpkgweb("test_web")
+
+  skip_if_packages_installed(web)
+
+  test_make(web, target_dir = "unrelated", lib_dir = "unrelated")
+
+  # Packages are not installed after running
+  expect_false(any((web %>% names) %in% rownames(installed.packages())))
+})
