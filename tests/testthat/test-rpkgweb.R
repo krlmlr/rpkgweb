005b24e08f25583679b8091f56204f3441fa5c29
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-13 19:16:02 +0200

    must specify root for rpkgweb() call, not subdir

--- tests/testthat/test-rpkgweb.R
+++ tests/testthat/test-rpkgweb.R
@@ -6,3 +6,13 @@ test_that("dirname = pkgname", {
   expect_error(rpkgweb("test_web_dirname"), "BBB != B")
   expect_error(rpkgweb("test_web_dirname"), "DD != D")
 })
+
+test_that("must specify root, not subdir in root", {
+  test_web_path <- normalize_path("test_web")
+
+  web <- test_web_path %>% rpkgweb
+  web %>% root_dir %>% expect_equal(test_web_path)
+  web <- test_web_path %>% file.path("AA") %>% rpkgweb
+  web %>% root_dir %>% expect_equal(test_web_path %>% file.path("AA"))
+  web %>% length %>% expect_equal(0)
+})
