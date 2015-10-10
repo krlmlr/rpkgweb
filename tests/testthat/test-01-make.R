880a38fab2543598ec3c0abd0225e7f88d33e88f
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-10 03:33:41 +0200

    Revert "remove all except for one failing test"
    This reverts commit 5cb7d501cfa4bbc5169b71dd64fa37a89092f702.
    
    Conflicts:
    	tests/testthat/helper-all.R

--- tests/testthat/test-01-make.R
+++ tests/testthat/test-01-make.R
@@ -3,6 +3,34 @@ context("make")
 
 web <- rpkgweb("test_web")
 
+test_that("dry run for default Makefile", {
+  test_make(web, dry_run = TRUE)
+})
+
+test_that("dry run for Makefile in cwd", {
+  test_make(web, target_dir = ".", dry_run = TRUE)
+})
+
+test_that("dry run for Makefile in other dir", {
+  test_make(web, target_dir = "unrelated", dry_run = TRUE)
+})
+
+test_that("execution of Makefile for temp lib", {
+  with_temp_lib(test_make(web))
+
+  # Packages are not installed after running
+  expect_false(any((web %>% names) %in% rownames(installed.packages())))
+})
+
+test_that("execution of Makefile for other lib", {
+  test_make(web, lib_dir = "unrelated")
+})
+
+test_that("execution of Makefile for different target_dir and lib in target_dir", {
+  # Not recommended!
+  test_make(web, target_dir = "unrelated", lib_dir = "unrelated")
+})
+
 test_that("execution of Makefile for different target_dir and other lib", {
   test_make(web, target_dir = "unrelated", lib_dir = "unrelated/lib")
 })
